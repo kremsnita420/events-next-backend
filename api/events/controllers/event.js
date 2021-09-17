890ed1434f3e1190d26734a63c2ecf4a81cde2ba
kremsnita420 <duranovic.safet@gmail.com>
2021-09-17 16:53:51 +0200

    login, register

---
+++ api/events/controllers/event.js
@@ -0,0 +1,30 @@
+'use strict';
+const { sanitizeEntity } = require('strapi-utils')
+
+
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+    // Get logged in users
+    async me(ctx) {
+        const user = ctx.state.user;
+
+        if (!user) {
+            return ctx.badRequest(null, [
+                { messages: [{ id: "No authorization header was found" }] },
+            ])
+        }
+
+        const data = await strapi.services.event.find({ user: user.id })
+
+        if (!data) {
+            return ctx.notFound();
+        }
+
+        return sanitizeEntity(data, { model: strapi.models.event })
+    },
+};
